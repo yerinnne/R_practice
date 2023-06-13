35062d504c5bb3ee1d4fff8cf116751e7487c3fa
yerinnne <annie3493@gachon.ac.kr>
2023-06-13 13:57:30 +0900

    ver1 commit

--- project_블로그 크롤링.py
+++ project_블로그 크롤링.py
@@ -33,7 +33,7 @@ def crawl_naver_blog(keywords):
         a = res.json().get('items')
         return a
     
-    # 여러 키워드 크롤링
+# 여러 키워드 크롤링
 keywords = ['서울 숙소 추천','서울 맛집 추천','서울 액티비티 추천',
             '강원 숙소 추천', "강원 맛집 추천", '강원 액티비티 추천',
             '제주 숙소 추천', "제주 맛집 추천", '제주 액티비티 추천',
@@ -45,7 +45,7 @@ key_list =[]
 for keyword in keywords:
     key_list.append(crawl_naver_blog(keyword))
 
-    import re
+import re
 b=[]
 for i in range(len(key_list)):
     # 특수문자를 제거한 모든 title
@@ -63,6 +63,7 @@ for i in range(len(key_list)):
     b.append(pd.DataFrame(d))
 
     df=pd.DataFrame()
+
 df['서울숙소']=b[0]; df['서울맛집']=b[1]; df['서울관광']=b[2];
 df['강원숙소']=b[3]; df['강원맛집']=b[4]; df['강원관광']=b[5]; 
 df['제주숙소']=b[6]; df['제주맛집']=b[7]; df['제주관광']=b[8];
